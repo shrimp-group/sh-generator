5e354f9ec2d124b5b4611bc768ac9a1bd6a9d926
wangkc <wkclz@qq.com>
2026-01-11 14:34:07 +0800

    数据源维护

--- generator-server/src/main/java/com/wkclz/generator/server/rest/GenDatasourceRest.java
+++ generator-server/src/main/java/com/wkclz/generator/server/rest/GenDatasourceRest.java
@@ -1,50 +1,82 @@
 package com.wkclz.generator.server.rest;
 
+import com.wkclz.core.base.PageData;
 import com.wkclz.core.base.R;
 import com.wkclz.core.enums.ResultCode;
 import com.wkclz.generator.server.Route;
 import com.wkclz.generator.server.bean.entity.GenDatasource;
+import com.wkclz.generator.server.service.GenDatasourceService;
+import com.wkclz.iam.sdk.helper.SessionHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(Route.PREFIX)
 public class GenDatasourceRest {
 
+    @Autowired
+    private GenDatasourceService genDatasourceService;
 
 
     @GetMapping(Route.DATASOURCE_PAGE)
     public R datasourcePage(GenDatasource entity) {
-        return R.ok();
+        PageData<GenDatasource> page = genDatasourceService.getGenDatasourcePage(entity);
+        return R.ok(page);
     }
 
     @GetMapping(Route.DATASOURCE_DETAIL)
     public R datasourceDetail(GenDatasource entity) {
         Assert.notNull(entity.getId(), ResultCode.PARAM_NO_ID.getMessage());
-        return R.ok();
+        GenDatasource detail = genDatasourceService.selectById(entity.getId());
+        return R.ok(detail);
     }
 
     @PostMapping(Route.DATASOURCE_CREATE)
     public R datasourceCreate(@RequestBody GenDatasource entity) {
-        return R.ok();
+        paramCheck(entity);
+        int insert = genDatasourceService.insert(entity);
+        return R.ok(insert);
     }
 
     @PostMapping(Route.DATASOURCE_UPDATE)
     public R datasourceUpdate(@RequestBody GenDatasource entity) {
-        Assert.notNull(entity.getId(), ResultCode.PARAM_NO_ID.getMessage());
-        Assert.notNull(entity.getId(), ResultCode.UPDATE_NO_VERSION.getMessage());
-        return R.ok();
+        paramCheck(entity);
+        int i = genDatasourceService.updateById(entity);
+        return R.ok(i);
     }
 
     @PostMapping(Route.DATASOURCE_REMOVE)
     public R datasourceRemove(@RequestBody GenDatasource entity) {
         Assert.notNull(entity.getId(), ResultCode.PARAM_NO_ID.getMessage());
-        return R.ok();
+        int i = genDatasourceService.deleteById(entity);
+        return R.ok(i);
     }
 
     @GetMapping(Route.DATASOURCE_OPTIONS)
     public R datasourceOptions() {
-        return R.ok();
+        List<GenDatasource> list = genDatasourceService.getDatasourceOptions();
+        return R.ok(list);
     }
+
+
+    private void paramCheck(GenDatasource entity) {
+        if (entity.getId() == null) {
+            entity.setUserCode(SessionHelper.getUserCode());
+            Assert.notNull(entity.getDbPassword(), "dbPassword 不能为空");
+        } else {
+            Assert.notNull(entity.getDbCode(), "dbCode 不能为空");
+            Assert.notNull(entity.getId(), ResultCode.PARAM_NO_ID.getMessage());
+            Assert.notNull(entity.getId(), ResultCode.UPDATE_NO_VERSION.getMessage());
+        }
+        Assert.notNull(entity.getDbType(), "dbType 不能为空");
+        Assert.notNull(entity.getDbHost(), "dbHost 不能为空");
+        Assert.notNull(entity.getDbPort(), "dbPort 不能为空");
+        Assert.notNull(entity.getDbSchema(), "dbSchema 不能为空");
+        Assert.notNull(entity.getDbUsername(), "dbUsername 不能为空");
+    }
+
 
 }
--- generator-server/src/main/java/com/wkclz/generator/server/service/GenDatasourceService.java
+++ generator-server/src/main/java/com/wkclz/generator/server/service/GenDatasourceService.java
@@ -1,11 +1,24 @@
 package com.wkclz.generator.server.service;
 
+import com.wkclz.core.base.PageData;
 import com.wkclz.generator.server.bean.entity.GenDatasource;
 import com.wkclz.generator.server.mapper.GenDatasourceMapper;
+import com.wkclz.mybatis.helper.PageQuery;
 import com.wkclz.mybatis.service.BaseService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class GenDatasourceService extends BaseService<GenDatasource, GenDatasourceMapper> {
+
+    public PageData<GenDatasource> getGenDatasourcePage(GenDatasource entity) {
+        return PageQuery.page(entity, mapper::getDatasourceList);
+    }
+
+    public List<GenDatasource> getDatasourceOptions() {
+        return mapper.getDatasourceOptions();
+
+    }
 
 }
